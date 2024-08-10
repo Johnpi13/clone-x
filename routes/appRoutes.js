e86b3a26e385469e9b4edb4950d604eb9590fb36
J. Alejandro <john.tamayo@correo.tdea.edu.co>
2024-08-10 11:14:35 -0500

    Fixing problems with files location

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -1,12 +1,9 @@
 const express = require("express")
 const router = express.Router()
 
-const { registerUser } = require("../controllers/registerController")
+const { registerUser } = require("../controllers/registerController.js")
 const { signUpUser } = require("../controllers/signUpController")
 const { publishTweet } = require("../controllers/tweetController")
-const { countFollowers } = require("../controllers/usersFollowersController")
-
-router.get("/posts", listPosts)
-router.get("/users", listUsers)
+const { countFollowers } = require("../controllers/userFollowersController")
 
 module.exports = router
