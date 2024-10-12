3b36b55f88ffcc9f33c664b17c6dec23c840bbe2
deivis herrera <herrera.15cortes@gmail.com>
2024-10-11 22:36:00 -0500

    fixing login

--- config/db.js
+++ config/db.js
@@ -1,31 +1,16 @@
-
 const mongoose = require('mongoose');
-
-let db;
-
-
+ 
 const connectBd = async () => {
   try {
-    await mongoose.connect(process.env.DB_CONNECTION_STRING, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    db = mongoose.connection; 
+    await mongoose.connect(process.env.DB_CONNECTION_STRING);
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection error:', error.message);
     process.exit(1);
   }
 };
-
-const getCollection = (collectionName) => {
-  if (!db) {
-    throw new Error('Database connection not established');
-  }
-  return db.collection(collectionName); 
-};
-
+ 
+ 
 module.exports = {
-  connectBd,
-  getCollection,
+  connectBd
 };
--- services/userService.js
+++ services/userService.js
@@ -1,39 +1,35 @@
 const User = require('../models/userSchema');
-const { getCollection } = require('../config/db');
-
+ 
 const getUser = async (params) => {
   try {
-    const userCollection = getCollection('users')
-    
-
     let conditions = []
     if (!!params.email) {
       conditions.push({ email: params.email })
     }
-
+ 
     if (!!params.userName) {
       conditions.push({ userName: params.userName })
     }
-
-    const user = await userCollection.findOne({
+ 
+    const user = await User.findOne({
       $or: conditions
     });
-    
+   
     return user;
   }
   catch (err) {
     console.log('something went wrong on getUser - userService');
     console.log(err);
-    
+   
     return null;
   }
 };
-
+ 
 const createUser = async (userData) => {
   try {
-    const user = new User(userData); 
-    await user.save(); 
-
+    const user = new User(userData);
+    await user.save();
+ 
     return {
       success: true,
       user,
